d40796af3e733da83dd47828b521e9a8c11f3673
Edwin Koffa <94025665+EdKoffa@users.noreply.github.com>
2022-12-14 21:24:17 -0500

    Basic HTML assignment

---
+++ BasicHTML/sampleimages/practice.js
@@ -0,0 +1,14 @@
+// A
+var num1 = 2
+console.log(num1+3)
+// B
+var num2 = 2
+num2 = num2 + 3
+console.log(num2)
+// C
+function add(num3, num4) {
+    sum = num3 + num4
+    console.log(sum)
+    return sum
+}
+add(2,3)
