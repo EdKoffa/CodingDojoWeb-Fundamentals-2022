ce8ccace793bb9b0aeaebe4eac99346b41162d5e
Edwin Koffa <94025665+EdKoffa@users.noreply.github.com>
2024-01-06 17:05:02 -0500

    Add files via upload

---
+++ Likes/script.js
@@ -0,0 +1,31 @@
+var thecurr = document.querySelector(".p1num");
+var newCurr = (thecurr.innerHTML);
+
+function addLike() {
+    newCurr++;
+
+    console.log(newCurr);
+    document.querySelector(".p1num").innerHTML = newCurr;
+}
+
+
+var thecurr1 = document.querySelector(".p2num");
+var newCurr1 = (thecurr1.innerHTML);
+
+function addLike1() {
+    newCurr1++;
+
+    console.log(newCurr1);
+    document.querySelector(".p2num").innerHTML = newCurr1;
+} 
+
+
+var thecurr2 = document.querySelector(".p3num");
+var newCurr2 = (thecurr2.innerHTML);
+
+function addLike2() {
+    newCurr2++;
+
+    console.log(newCurr2);
+    document.querySelector(".p3num").innerHTML = newCurr2;
+}
